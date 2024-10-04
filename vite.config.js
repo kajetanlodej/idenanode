3e9a2f86c9992e35fc4a96dd771af894eab6dba5
Kajetan Łodej <kajetan.lodej@gmail.com>
2024-10-05 00:23:09 +0200

    Check build setting

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    rollupOptions: {
+      external: ['./src/assets/simplifiedMap.json']
+    }
+  }
 })
